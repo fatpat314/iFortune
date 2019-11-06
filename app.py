a2b64fbd515be40f8899dba01d358af870153fee
Patrick <w.patrick.kelly@gmail.com>
2019-11-05 17:29:03 -0800

    Finished

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -16,19 +16,66 @@ def fortune():
 @app.route('/fortune_results')
 def fortune_results():
     """Displays the user's fortune."""
+
+    # NAME
+    users_name = request.args.get('firstname')
+
+    # SIBLIST
+    users_siblist = request.args.get('siblist')
+
+    if users_siblist == '0':
+        sib_text = 'is an only child'
+
+    elif users_siblist == '1':
+        sib_text = 'has one sibling'
+
+    elif users_siblist == '2':
+        sib_text = 'has more siblings'
+
+    else:
+        sib_text = 'has a lot of siblings'
+
+    # ANIMALS
     users_favorite_animal = request.args.get('animal')
-    print("hello")
 
-    if users_favorite_animal == 'unicorn':
-        print("fortune is You'll have  magical day!")
-    
-    elif condition2:
-        pass
-    elif condition3:
-        pass
-    elif condition4:
+    if users_favorite_animal == 'Unicorn':
+        fortune_text = 'will have a magic day'
 
-        # no other fortune applies. return default fortune
-        pass
+    elif users_favorite_animal == 'Narwal':
+        fortune_text = 'is not a Unicorn'
 
-        return render_template('fortune_results.html')
+    else:
+        fortune_text = 'does not like horns'
+
+    #San Francico
+    users_cities = request.args.get('city1')
+
+    # if users_cities == 'San Francico' and 'New York' and 'Austin':
+    #     city_text = ' and has traveled a lot'
+
+    if users_cities == 'San Francisco':
+        city_text = ' and has been to San Francisco'
+    else:
+        city_text = ''
+    #New York
+    users_cities = request.args.get('city2')
+
+    if users_cities == 'New York':
+        city_text2 = ' and has been to New York'
+    else:
+        city_text2 = ''
+
+    #Austin
+    users_cities = request.args.get('city3')
+    if users_cities == 'Austin':
+        city_text3 = ' and Austin'
+    else:
+        city_text3 = ''
+
+    if users_cities == None:
+        city_text = ' and needs to get out more'
+    # else:
+    #     city_text = ' and needs to get out more.'
+
+
+    return render_template('fortune_results.html', fortune_results = users_name + " " + fortune_text + ", " + sib_text + city_text + city_text2 + city_text3)
